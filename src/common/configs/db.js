dddedc69ab88af4ca128f1aee4087da1062bb5cb
TCSon03 <truongcongsson@gmail.com>
2025-07-06 15:24:43 +0700

    Làm lại tất cả

--- src/common/configs/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import { DB_URI } from "./enviroments.js";
-
-function connectDB() {
-  return mongoose.connect(DB_URI); // return Promise
-}
-
-export default connectDB;
--- src/common/utils/error.js
+++
@@ -1,6 +0,0 @@
-const createError = (statusCode, message) => {
-    const error = new Error(message || "INTERNAL SERVER ERROR");
-    error.statusCode = statusCode || 500;
-    return error;
-}
-export default createError;
