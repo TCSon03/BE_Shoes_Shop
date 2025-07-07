2d085e2554a1be24c9ef85152494bd80ac398602
TCSon03 <truongcongsson@gmail.com>
2025-07-07 11:48:23 +0700

    Tach rieng bien moi truong env

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 import express from "express";
-import dotenv from "dotenv";
 import connectDB from "./src/common/configs/db.js";
 import router from "./src/router/index.js";
-
-// Load biến môi trường từ .env
-dotenv.config();
+import { PORT } from "./src/common/configs/environments.js";
 
 // Kết nối database
 connectDB();
@@ -17,8 +14,6 @@ app.use(express.json());
 
 // Router
 app.use("/api", router);
-
-const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
